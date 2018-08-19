2cc2d86bb30df19581df99a553a6911e1fadc697
juju4 <juju4@users.noreply.github.com>
2018-08-19 19:03:18 -0400

    fix typo

--- get-puppetmodules.sh
+++ get-puppetmodules.sh
@@ -16,7 +16,7 @@ m=modules
 [ ! -d $m/puppetlabs-stdlib ] && git clone https://github.com/puppetlabs/puppetlabs-stdlib.git $m/stdlib
 [ ! -d $m/hardening-stdlib ] && git clone https://github.com/dev-sec/puppet-hardening-stdlib $m/hardening-stdlib
 [ ! -d $m/os_hardening ] && git clone https://github.com/dev-sec/puppet-os-hardening $m/os_hardening
-[ ! -d $m/ssh_hardening ] && git clone https://github.com/dev-sec/puppet-os-hardening $m/ssh_hardening
+[ ! -d $m/ssh_hardening ] && git clone https://github.com/dev-sec/puppet-ssh-hardening $m/ssh_hardening
 [ ! -d $m/thias-sysctl ] && git clone https://github.com/thias/puppet-sysctl.git $m/sysctl
 [ ! -d $m/firewall ] && git clone https://github.com/puppetlabs/puppetlabs-firewall $m/firewall
 #[ ! -d $m/cisecurity ] && git clone https://github.com/cohdjn/cisecurity $m/cohdjn-cisecurity
