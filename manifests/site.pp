b37833814629a88d1feeee7d9d2876b5485fec53
juju4 <juju4@users.noreply.github.com>
2018-08-25 15:11:34 -0400

    fix sshd config: deprecated option, vagrant required MACs

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,9 +23,11 @@
     'UsePrivilegeSeparation'    => 'sandbox',
     'KexAlgorithms'             => 'curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256',
     'Ciphers'                   => 'chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr',
-    'MACs'                      => 'hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256',
+#    'MACs'                      => 'hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256',
+# for vagrant
+    'MACs'                      => 'hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com',
     'Banner'                    => '',
-    'UseRoaming'                => 'no',
+#    'UseRoaming'                => 'no',   # deprecated
   }
   $client_options         = {
     'KexAlgorithms'             => 'curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256',
