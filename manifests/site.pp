bc4af6e708c161e1dc7df378e66a023ca8800e05
juju4 <juju4@users.noreply.github.com>
2018-09-09 16:20:39 -0400

    use content for profile input instead of source

--- manifests/site.pp
+++ manifests/site.pp
@@ -243,7 +243,22 @@
 
   file { '/etc/profile.d/security':
     ensure => present,
-    source => 'file:///tmp/kitchen/files/profile.erb',
+    #source => 'file:///tmp/kitchen/files/profile.erb',
+    content => "### PUPPET MANAGED BLOCK: bash settings ###
+      export HISTCONTROL=
+      export HISTFILE=$HOME/.bash_history
+      export HISTFILESIZE=5000
+      export HISTIGNORE=
+      export HISTSIZE=3000
+      export HISTTIMEFORMAT="%a %b %Y %T %z "
+      if [ "X$SHELL" = '/bin/bash' ]; then
+        typeset -r HISTCONTROL
+        typeset -r HISTFILE
+        typeset -r HISTFILESIZE
+        typeset -r HISTIGNORE
+        typeset -r HISTSIZE
+        typeset -r HISTTIMEFORMAT
+      fi",
   }
 
   accounts::user { 'jeff':
