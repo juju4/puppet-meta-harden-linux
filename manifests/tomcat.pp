d9b84780bb9f051258f09dc61617765c54fdd05a
juju4 <juju4@users.noreply.github.com>
2018-11-10 14:54:01 -0500

    tomcat: more systemd security options

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -133,6 +133,11 @@ MemoryLimit=8G
 PrivateTmp=yes
 InaccessibleDirectories=/home
 ReadOnlyDirectories=/var
+NoNewPrivileges=yes
+ProtectHome=read-only
+ProtectKernelTunables=true
+ProtectSystem=full
+RestrictRealtime=true
 
 [Install]
 WantedBy=multi-user.target",
