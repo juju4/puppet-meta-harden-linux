bc4e1979f3af23512b02de7161d7819603758bbf
juju4 <juju4@users.noreply.github.com>
2019-12-21 14:44:21 -0500

    get-puppetmodules: review modules dependencies

--- get-puppetmodules.sh
+++ get-puppetmodules.sh
@@ -14,13 +14,14 @@ m=modules
 #[ ! -d $m/firewall ] && git clone https://github.com/puppetlabs/puppetlabs-firewall $m/puppetlabs-firewall
 #[ ! -d $m/cisecurity ] && git clone https://github.com/cohdjn/cisecurity $m/cohdjn-cisecurity
 
-[ ! -d $m/stdlib ] && git clone https://github.com/puppetlabs/puppetlabs-stdlib.git $m/stdlib --branch 4.25.1
+[ ! -d $m/stdlib ] && git clone https://github.com/puppetlabs/puppetlabs-stdlib.git $m/stdlib
 # https://tickets.puppetlabs.com/browse/MODULES-2145
 [ ! -d $m/apt ] && git clone --config transfer.fsckobjects=false --config fetch.fsckobjects=false --config receive.fsckobjects=false https://github.com/puppetlabs/puppetlabs-apt.git $m/apt --branch 5.0.1
 [ ! -d $m/translate ] && git clone https://github.com/puppetlabs/puppetlabs-translate.git $m/translate
 [ ! -d $m/accounts ] && git clone https://github.com/puppetlabs/puppetlabs-accounts.git $m/accounts
 [ ! -d $m/hardening-stdlib ] && git clone https://github.com/dev-sec/puppet-hardening-stdlib $m/hardening-stdlib
 [ ! -d $m/os_hardening ] && git clone https://github.com/dev-sec/puppet-os-hardening $m/os_hardening
+[ ! -d $m/augeasproviders_sysctl ] && git clone https://github.com/hercules-team/augeasproviders_sysctl.git $m/augeasproviders_sysctl
 [ ! -d $m/ssh_hardening ] && git clone https://github.com/dev-sec/puppet-ssh-hardening $m/ssh_hardening
 [ ! -d $m/ssh ] && git clone https://github.com/saz/puppet-ssh $m/ssh
 [ ! -d $m/thias-sysctl ] && git clone https://github.com/thias/puppet-sysctl.git $m/sysctl
@@ -35,7 +36,7 @@ m=modules
 [ ! -d $m/puppetlabs-concat ] && git clone https://github.com/puppetlabs/puppetlabs-concat.git $m/concat --branch 4.2.1
 [ ! -d $m/epel ] && git clone https://github.com/jordiprats/eyp-epel.git $m/epel
 [ ! -d $m/fail2ban ] && git clone https://github.com/voxpupuli/puppet-fail2ban.git $m/fail2ban
-[ ! -d $m/extlib ] && git clone https://github.com/voxpupuli/puppet-extlib.git $m/extlib --branch v2.3.1
+[ ! -d $m/extlib ] && git clone https://github.com/voxpupuli/puppet-extlib.git $m/extlib
 [ ! -d $m/rkhunter ] && git clone https://github.com/itmanagerro/puppet-rkhunter.git $m/rkhunter
 [ ! -d $m/telegraf ] && git clone https://github.com/voxpupuli/puppet-telegraf.git $m/telegraf
 [ ! -d $m/postfix ] && git clone https://github.com/NTTCom-MS/eyp-postfix.git $m/postfix
@@ -44,6 +45,6 @@ m=modules
 [ ! -d $m/smarthost ] && git clone https://github.com/justinjl6/puppet-smarthost.git $m/smarthost
 [ ! -d $m/resolvconf ] && git clone https://github.com/suchpuppet/puppet-resolvconf.git $m/resolvconf
 [ ! -d $m/timezone ] && git clone https://github.com/saz/puppet-timezone.git $m/timezone
-[ ! -d $m/debconf ] && git clone https://github.com/smoeding/puppet-debconf.git $m/debconf
+[ ! -d $m/debconf ] && git clone https://github.com/smoeding/puppet-debconf.git $m/debconf --branch v2.3.0
 [ ! -d $m/logrotate ] && git clone https://github.com/voxpupuli/puppet-logrotate.git $m/logrotate
 [ ! -d $m/selinux ] && git clone https://github.com/voxpupuli/puppet-selinux.git $m/selinux
